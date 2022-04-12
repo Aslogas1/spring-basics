1fb89cc744184b22ff7d9ed4553b128c57055208
Alexandr <probatanoob@mail.ru>
2022-04-12 17:23:38 +0300

    Entities added

--- src/main/java/main/DefaultController.java
+++ src/main/java/main/DefaultController.java
@@ -1,16 +1,35 @@
 package main;
 
+import main.model.ToDo;
+import main.model.ToDoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
-@RestController
-
+@Controller
 public class DefaultController {
-@RequestMapping("/")
-    public String index() {
-    Random random = new Random();
-        return String.valueOf(random.nextInt());
+    @Autowired
+    private final ToDoRepository toDoRepository;
+
+    public DefaultController(ToDoRepository toDoRepository) {
+        this.toDoRepository = toDoRepository;
+    }
+
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        Iterable<ToDo> toDoIterable = toDoRepository.findAll();
+        List<ToDo> toDos = new ArrayList<>();
+
+        for (ToDo toDo : toDoIterable) {
+            toDos.add(toDo);
+        }
+        model.addAttribute("todos", toDos)
+                .addAttribute("todosCount", toDos.size());
+        return "index";
     }
 }
